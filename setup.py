32d372d9af1d0605b736d8f72e90ff64c6d7f5aa
Valentin Herrmann <accounts@valentin-herrmann.com>
2025-11-21 19:28:47 +0100

    V 0.2.12

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='sql_testing_tools',
-    version='0.2.11',
+    version='0.2.12',
     packages=find_packages(),
     install_requires=[
         'sqlparse>=0.5.1',
